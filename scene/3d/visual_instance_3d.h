196497a307708914aa60fbbab3a37d877c97be0b
Henry Heffernan <henryheffernan@gmail.com>
2026-05-22 01:34:38 -0700

    P1b: GeometryInstance3D.static_shadow_caster property -> instance_geometry_set_flag(INSTANCE_FLAG_STATIC_SHADOW_CASTER)

--- scene/3d/visual_instance_3d.h
+++ scene/3d/visual_instance_3d.h
@@ -141,6 +141,7 @@ private:
 	float lightmap_texel_scale = 1.0f;
 	GIMode gi_mode = GI_MODE_STATIC;
 	bool ignore_occlusion_culling = false;
+	bool static_shadow_caster = true;
 
 	const StringName *_instance_uniform_get_remap(const StringName &p_name) const;
 
@@ -205,6 +206,9 @@ public:
 
 	void set_ignore_occlusion_culling(bool p_enabled);
 	bool is_ignoring_occlusion_culling();
+
+	void set_static_shadow_caster(bool p_enabled);
+	bool is_static_shadow_caster() const;
 
 	virtual Ref<TriangleMesh> generate_triangle_mesh() const;
 
